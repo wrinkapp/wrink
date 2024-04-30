8f4707626d9ac1e3bf2fc1201872d08322586fc4
Kyle Dickey <41174949+dickeyy@users.noreply.github.com>
2024-04-29 23:19:39 -0600

    fix some styles

--- apps/website/components/navbar.tsx
+++ apps/website/components/navbar.tsx
@@ -4,12 +4,12 @@ import { Button } from "./ui/button";
 
 export default function Navbar() {
     return (
-        <nav className="absolute top-2 flex w-[97%] flex-row items-center justify-between px-3 py-1">
+        <nav className="absolute top-2 flex w-full flex-row items-center justify-between px-6 py-1">
             <div className="flex flex-row items-center gap-2">
                 <Image
                     src="/logo.png"
-                    width={30}
-                    height={30}
+                    width={32}
+                    height={32}
                     alt="Wrink Logo"
                     className="rounded border"
                 />
@@ -18,18 +18,18 @@ export default function Navbar() {
 
             <div className="flex flex-row items-center gap-4">
                 <ul className="flex flex-row items-center gap-4">
-                    <li className="text-foreground/80 hover:text-foreground text-sm  transition-all duration-200 ease-in-out">
+                    <li className="text-foreground/80 hover:text-foreground text-md  transition-all duration-200 ease-in-out">
                         <Link href="/pricing">Developers</Link>
                     </li>
-                    <li className="text-foreground/80 hover:text-foreground text-sm  transition-all duration-200 ease-in-out">
+                    <li className="text-foreground/80 hover:text-foreground text-md  transition-all duration-200 ease-in-out">
                         <Link href="/pricing">Blog</Link>
                     </li>
-                    <li className="text-foreground/80 hover:text-foreground text-sm  transition-all duration-200 ease-in-out">
+                    <li className="text-foreground/80 hover:text-foreground text-md  transition-all duration-200 ease-in-out">
                         <Link href="/pricing">Pricing</Link>
                     </li>
                 </ul>
 
-                <Button size="sm">Get Started</Button>
+                <Button>Get Started</Button>
             </div>
         </nav>
     );
