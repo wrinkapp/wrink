447ab90535f2d0819f56588195c6fa4969b1c030
Kyle Dickey <41174949+dickeyy@users.noreply.github.com>
2024-05-01 00:39:45 -0600

    changed landing and logo and metadata

--- apps/website/app/page.tsx
+++ apps/website/app/page.tsx
@@ -2,10 +2,15 @@ import Navbar from "@/components/navbar";
 
 export default function Page() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center overflow-y-hidden px-6 sm:px-24">
+        <main className="flex min-h-screen flex-col items-center justify-center overflow-y-hidden px-4 sm:px-24">
             <Navbar />
-            <h1 className="text-4xl font-bold">Get sh*t done.</h1>
-            <p className="text-foreground/40 mt-4 text-lg">coming soon...</p>
+            <div className="flex flex-col items-center justify-center gap-2">
+                <div className="bg-muted/20 mb-6 flex rounded-full border px-4 sm:mb-12">
+                    <p className="text-foreground/30 text-lg">coming soon</p>
+                </div>
+                <h1 className="text-4xl font-extrabold sm:text-5xl">Get sh*t done.</h1>
+                <p className="text-foreground/60 text-lg">A new wave of project management.</p>
+            </div>
         </main>
     );
 }
--- apps/website/components/navbar.tsx
+++ apps/website/components/navbar.tsx
@@ -4,19 +4,19 @@ import { Button } from "./ui/button";
 
 export default function Navbar() {
     return (
-        <nav className="absolute top-2 flex w-full flex-row items-center justify-between px-6 py-1">
+        <nav className="absolute top-2 flex w-full flex-row items-center justify-between px-4 py-1 sm:px-6">
             <div className="flex flex-row items-center gap-2">
                 <Image
                     src="/logo.png"
-                    width={32}
-                    height={32}
+                    width={35}
+                    height={35}
                     alt="Wrink Logo"
                     className="rounded border"
                 />
-                <p className="text-xl font-bold">Wrink</p>
+                <p className="text-xl font-extrabold">Wrink</p>
             </div>
 
-            <div className="flex flex-row items-center gap-4">
+            <div className="hidden flex-row items-center gap-4 sm:flex">
                 <ul className="flex flex-row items-center gap-4">
                     <li className="text-foreground/80 hover:text-foreground text-md  transition-all duration-200 ease-in-out">
                         <Link href="/pricing">Developers</Link>
@@ -29,7 +29,7 @@ export default function Navbar() {
                     </li>
                 </ul>
 
-                <Button>Get Started</Button>
+                <Button className="h-fit min-h-0 py-[0.4rem]">Get Started</Button>
             </div>
         </nav>
     );
